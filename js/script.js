3f3b47177c0f29017175fe91d2f768bb6cf1a60a
Nadja Brandys <nadiusza@gmail.com>
2019-12-09 22:16:40 +0100

    Add rest of the player moves

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ if(playerInput == '1'){
 }
   else if(playerInput== '2'){
 	playerMove= 'nożyce';
-	
+
 } else {
 	playerMove='papier';
 
@@ -18,6 +18,35 @@ if(playerInput == '1'){
 
 printMessage('Twój ruch to: ' + playerMove);
 
-
-
-
+if(computerMove=='kamień' && playerMove =='papier'){
+		printMessage('Ty wygrywasz');
+} else if 
+	(computerMove == "papier" && playerMove =='papier'){
+		printMessage('Remis');
+	} else if 
+	(computerMove =='papier' && playerMove=='kamień'){
+		printMessage('Komputer wygrywa');
+	} else if 
+	(computerMove =='papier' && playerMove=='nożyce'){
+		printMessage('Ty wygrywasz');
+		}	else if
+		(computerMove =='kamień' && playerMove=='nożyce'){
+		printMessage('Komputer wygrywa');
+	} else if
+	(computerMove =='kamień' && playerMove=='kamień'){
+		printMessage('Remis');
+	} else if 
+	(computerMove =='nożyce' && playerMove=='kamień'){
+		printMessage('Ty wygrywasz');
+	} else if (computerMove =='nożyce' && playerMove=='nożyce'){
+		printMessage('Remis');
+	} else if (computerMove =='nożyce' && playerMove=='papier'){
+		printMessage('Komputer wygrywa');
+	} else if (computerMove == 'papier' && playerMove!= ['1', '2', '3']){
+		printMessage('Nieznany ruch');
+	}else if (computerMove == 'kamień' && playerMove!= ['1', '2', '3']){
+			printMessage('Nieznany ruch');
+	}else
+	(computerMove == 'nożyce' && playerMove!= ['1', '2', '3']){
+				printMessage('Nieznany ruch');
+	}
